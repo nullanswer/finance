3f639524cd0d928a8dba5515727f722c88348396
유관우 <yugwan-u@yugwan-uui-MacBook-Pro.local>
2017-08-09 16:59:53 +0900

    xx

--- client/main.js
+++ client/main.js
@@ -1,22 +1,39 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Mongo } from 'meteor/mongo'
 import './main.html';
+
+Messages = new Mongo.Collection('messages');
+Meteor.subscribe('messages');
+
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.messages.helpers({
+    messages : messageFunc,
+    time : timeFunc
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.newMessage.events({
+    "submit from" : createMessage
 });
+
+function messageFunc() {
+    return Messages.find({},{sort: {'time': -1} });
+}
+
+function timeFunc(time) {
+    return moment(time).format("HH:mm:ss");
+}
+
+function createMessage(evt) {
+    Messages.insert({
+        message : evt.target.message.value,
+        time : new Date()
+    });
+    evt.target.message.value = "";
+    return false;
+}
--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+Messages = new Mongo.Collection('messages');
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+});
+
+Meteor.publish('messages', function taskPublication() {
+    return Messages.find();
 });
